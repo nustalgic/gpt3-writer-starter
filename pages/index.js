913524086d9d150e25b14a95dfc967af92bfb2a9
nustalgic <95715579+nustalgic@users.noreply.github.com>
2024-02-10 05:44:54 +0000

    interface updates

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,45 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  };
+  <textarea
+  className="prompt-box"
+  placeholder="start typing here"
+  value={userInput}
+  onChange={onUserChangedText}
+  />;
   return (
     <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>ask, and we'll expand</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>write something you want to discuss and we'll turn it into a thread for socials</h2>
           </div>
+        </div>
+      </div>
+      <div className="prompt-container">
+        <textarea
+          placeholder="start typing here"
+          className="prompt-box"
+          value={userInput}
+          onChange={onUserChangedText}
+        />
+        {/* New code I added here */}
+        <div className="prompt-buttons">
+          <a className="generate-button" onClick={null}>
+            <div className="generate">
+              <p>Generate</p>
+            </div>
+          </a>
         </div>
       </div>
       <div className="badge-container grow">
