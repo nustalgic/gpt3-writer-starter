e7735e7f6a84b5f543f465338707219a3237845e
nustalgic <95715579+nustalgic@users.noreply.github.com>
2024-02-10 06:50:57 +0000

    updates after local clone

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,28 @@ import { useState } from 'react';
 
 const Home = () => {
   const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+const [isGenerating, setIsGenerating] = useState(false)
+
+const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+  
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
   const onUserChangedText = (event) => {
     setUserInput(event.target.value);
   };
@@ -27,21 +49,33 @@ const Home = () => {
         </div>
       </div>
       <div className="prompt-container">
-        <textarea
-          placeholder="start typing here"
-          className="prompt-box"
-          value={userInput}
-          onChange={onUserChangedText}
-        />
-        {/* New code I added here */}
-        <div className="prompt-buttons">
-          <a className="generate-button" onClick={null}>
-            <div className="generate">
-              <p>Generate</p>
-            </div>
-          </a>
+      <textarea
+        placeholder="start typing here"
+        className="prompt-box"
+        value={userInput}
+        onChange={onUserChangedText}
+      />
+      <div className="prompt-buttons">
+        <a className="generate-button" onClick={callGenerateEndpoint}>
+          <div className="generate">
+            <p>Generate</p>
+          </div>
+        </a>
+      </div>
+      {/* New code I added here */}
+      {apiOutput && (
+      <div className="output">
+        <div className="output-header-container">
+          <div className="output-header">
+            <h3>Output</h3>
+          </div>
+        </div>
+        <div className="output-content">
+          <p>{apiOutput}</p>
         </div>
       </div>
+    )}
+    </div>
       <div className="badge-container grow">
         <a
           href="https://buildspace.so/builds/ai-writer"
